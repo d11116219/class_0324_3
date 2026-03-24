f7123a26a616e7729b0f01006a505b277553e073
d11116219 <126222904+d11116219@users.noreply.github.com>
2026-03-24 16:15:27 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Test
+{
+    int Mid;
+    int Final;
+
+    public Test(int Mid, int Final)
+    {
+        this.Mid = Mid;
+        this.Final = Final;
+    }
+
+    public void SetMid(int Mid)
+    {
+        this.Mid = Mid;
+    }
+
+    public void SetFinal(int Final)
+    {
+        this.Final = Final;
+    }
+
+    public int GetMid()
+    {
+        return Mid;
+    }
+
+    public int GetFinal()
+    {
+        return Final;
+    }
+
+    public double GetAvg()
+    {
+        return (Mid + Final) / 2.0;
+    }
+}
+
+~Test ()
+    {
+    Console.WriteLine("解構了");
+}
+    
---
+++ class_0324_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace class_0324_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
